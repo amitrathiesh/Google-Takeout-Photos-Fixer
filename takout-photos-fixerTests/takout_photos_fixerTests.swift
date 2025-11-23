f17dfea063f71f9a1fe17a06e5b3de023b30c79b
Amit Rathiesh <amitrathiesh@webzler.com>
2025-11-23 13:23:00 -0500

    Initial Commit

---
+++ takout-photos-fixerTests/takout_photos_fixerTests.swift
@@ -0,0 +1,17 @@
+//
+//  takout_photos_fixerTests.swift
+//  takout-photos-fixerTests
+//
+//  Created by Amit Rathiesh on 2025-11-23.
+//
+
+import Testing
+@testable import takout_photos_fixer
+
+struct takout_photos_fixerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
